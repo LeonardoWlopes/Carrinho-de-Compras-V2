df84457d2d90ec386d60e669c1a74a75a40b12ff
Leonardo William Lopes <62454440+LeonardoWlopes@users.noreply.github.com>
2022-05-18 12:31:47 -0300

    Feat: Created Theme

---
+++ src/themes/light.ts
@@ -0,0 +1,21 @@
+const light = {
+  COLORS: {
+    BACKGROUND: {
+      main: "#d1d8e5",
+      primary: "#ffffff",
+    },
+    TEXT: {
+      primary: "#000000",
+      secondary: "#a5a5ab",
+    },
+    GREEN: {
+      light: "#c7ffa6",
+      dark: "#247a00",
+    },
+    BLUE: {
+      dark: "#3b74f2",
+    },
+  },
+};
+
+export default light;
